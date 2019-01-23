b85041023d9a42f741be1dec462a31491c616fbc
Cleber Miranda Barbosa <cleber@gmail.com>
2019-01-22 22:19:45 -0200

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,44 @@
+package boardgame;
+
+/**
+ * 
+ * @author Cleber Mirada Barbosa
+ *
+ */
+public class Position {
+	private int row;
+	private int column;
+	
+	public Position()  {
+		super();
+	}
+	
+	public Position(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row+","+column;
+	}
+	
+	
+}
---
+++ src/test/PositionTest.java
@@ -0,0 +1,78 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import boardgame.Position;
+
+public class PositionTest {
+
+
+	@Test
+	public void testGetRow() {
+		
+		//Scenario
+		Position position = new Position(3, 5);
+		
+		//Action 
+		int row = position.getRow();
+		
+		//Verification
+		Assert.assertTrue(row == 3);
+	}
+
+	@Test
+	public void testSetRow() {
+
+		//Scenario
+		Position position = new Position();
+		
+		//Action 
+		position.setRow(3);
+		int row = position.getRow();
+		
+		//Verification
+		Assert.assertTrue(row == 3);
+		
+	}
+
+	@Test
+	public void testGetColumn() {
+		//Scenario
+		Position position = new Position(3,5);
+		
+		//Action 
+		int column = position.getColumn();
+		
+		//Verification
+		Assert.assertTrue(column == 5);
+	}
+
+	@Test
+	public void testSetColumn() {
+		//Scenario
+		Position position = new Position();
+		
+		//Action 
+		position.setColumn(5);
+		int column = position.getColumn();
+		
+		//Verification
+		Assert.assertTrue(column == 5);
+	}
+
+	@Test
+	public void testToString() {
+		//Scenario
+		Position position = new Position(3,5);
+		
+		//Action 
+		String text = position.toString();
+		
+		//Verification
+		Assert.assertTrue(text.equals("3,5"));
+		
+		
+	}
+
+}
