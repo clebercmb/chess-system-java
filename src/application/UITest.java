9a789c5703841f48b870d5b6d35d0d6b9601bb58
Cleber Miranda Barbosa <cleber@gmail.com>
2019-01-25 21:59:07 -0200

    Moving pieces

--- src/application/UITest.java
+++ src/application/UITest.java
@@ -1,45 +1,76 @@
 package application;
 
+import java.io.ByteArrayInputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-
+import org.junit.Assert;
 import org.junit.Test;
 
 import boardgame.Board;
 import chess.ChessMatch;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UITest {
 
 	@Test
 	public void testPrintBoard() {
-		//Scenario
+		// Scenario
 		ChessMatch chessMatch = new ChessMatch();
-		
-		//Action / Validation
+
+		// Action / Validation
 		UI.printBoard(chessMatch.getPieces());
 	}
 
 	@Test
 	public void testPrintBoard2() {
-		//Scenario
+		// Scenario
 		ChessPiece[][] chessPiece = new ChessPiece[8][8];
-		
-		//Action / Validation
+
+		// Action / Validation
 		UI.printBoard(chessPiece);
 	}
-	
+
 	@Test
 	public void testPrintBoard3() {
-		//Scenario
+		// Scenario
 		ChessPiece[][] chessPiece = new ChessPiece[8][8];
-		Board board = new Board(8,8);
-		chessPiece[0][0] = new ChessPiece(board, Color.WHITE);		
+		Board board = new Board(8, 8);
+		chessPiece[0][0] = new ChessPiece(board, Color.WHITE);
 		UI ui = new UI();
 		ui.toString();
-		
-		//Action / Validation
+
+		// Action / Validation
 		UI.printBoard(chessPiece);
+	}
+
+	@Test
+	public void testReadChessPosition() {
+
+		//Scenario
+		String data = "a8";
+		System.setIn(new ByteArrayInputStream(data.getBytes()));
+		Scanner scanner = new Scanner(System.in);
+
+		// Action
+		ChessPosition chessPosition = UI.readChessPosition(scanner);
+
+		// Action / Validation
+		Assert.assertEquals("a8", chessPosition.toString());
+	}
+
+	@Test(expected = InputMismatchException.class)  
+	public void testReadChessPosition2() {
+
+		//Scenario
+		String data = "aa";
+		System.setIn(new ByteArrayInputStream(data.getBytes()));
+		Scanner scanner = new Scanner(System.in);
+
+		// Action and Validation
+		UI.readChessPosition(scanner);
 	}	
 	
 }
--- src/chess/ChessMatchTest.java
+++ src/chess/ChessMatchTest.java
@@ -2,6 +2,9 @@ package chess;
 import org.junit.Assert;
 import org.junit.Test;
 
+import application.UI;
+import boardgame.BoardException;
+
 public class ChessMatchTest {
 
 	@Test
@@ -33,6 +36,60 @@ public class ChessMatchTest {
 
 	}
 	
+	@Test
+	public void testPerformChessMove() {
+		//Scenario
+		ChessMatch chessMatch = new ChessMatch();
+		ChessPiece[][] chessPieces = chessMatch.getPieces();
+		ChessPiece pieceSource = chessPieces[1][2];
+
+		ChessPosition sourcePosition = new ChessPosition('c',7);
+		ChessPosition targetPosition = new ChessPosition('c',6);
+		
+		//Action
+		ChessPiece piece = chessMatch.performChessMove(sourcePosition, targetPosition);
+		ChessPiece pieceTarget = chessMatch.getPieces()[2][2];
+		
+		//Validation
+		Assert.assertEquals(pieceTarget, pieceSource );
+		Assert.assertNull(piece);
+
+	}
+	
+	@Test
+	public void testPerformChessMove2() {
+		//Scenario
+		ChessMatch chessMatch = new ChessMatch();
+		ChessPiece[][] chessPieces = chessMatch.getPieces();
+		ChessPiece pieceSource = chessPieces[0][3];
+		ChessPiece pieceTarget = chessPieces[7][4];
+
+		ChessPosition sourcePosition = new ChessPosition('d',8);
+		ChessPosition targetPosition = new ChessPosition('e',1);
+		
+		//Action
+		ChessPiece piece = chessMatch.performChessMove(sourcePosition, targetPosition);
+		ChessPiece pieceTarget2 = chessMatch.getPieces()[7][4];
+			
+		//Validation
+		Assert.assertEquals(pieceTarget2, pieceSource );
+		Assert.assertEquals(piece, pieceTarget);
+
+	}
+	
+
+	@Test(expected = ChessException.class)  
+	public void testPerformChessMove3() {
+		//Scenario
+		ChessMatch chessMatch = new ChessMatch();
+
+		ChessPosition sourcePosition = new ChessPosition('c',5);
+		ChessPosition targetPosition = new ChessPosition('e',1);
+		
+		//Action and Validation
+		chessMatch.performChessMove(sourcePosition, targetPosition);
+
+	}
 	
 	
 }
